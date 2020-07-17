b64e2baa0d0eecb0b971171fe4c046414f06bd04
Tereh <lositsky@outlook.com>
2020-07-17 22:40:16 +0300

    зміта дати таймера

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ import cards from "./modules/cards";
 import calc from "./modules/calc";
 import forms from "./modules/forms";
 import slider from "./modules/slider";
-import { openModal } from "./modules/modal";
+import {
+  openModal
+} from "./modules/modal";
 
 window.addEventListener("DOMContentLoaded", function () {
   const modalTimerId = setTimeout(
